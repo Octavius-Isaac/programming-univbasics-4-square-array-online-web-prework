b30f134a2627da8266e663fa0d0db5863a9567f6
Solomon Octavius Isaac <octavius.isaac@gmail.com>
2019-10-02 04:44:27 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,6 +1,7 @@
 def square_array(array)
   new_array = []
   number = 0
+<<<<<<< HEAD
   while number < array.length do 
     squared = array[number]**2 
     puts squared 
@@ -8,4 +9,11 @@ def square_array(array)
     number = number+1 
   end 
   return new_array
+=======
+  while number <= array.length do 
+    squared = array[number]**2 
+    puts squared 
+    number = number+1 
+  end 
+>>>>>>> 6a67c0282400eac32bae3aa1fa011a3f84c41a0a
 end 
